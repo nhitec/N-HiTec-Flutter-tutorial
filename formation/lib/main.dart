c95921cd8c3dd0b1d0410ba919bc8c68fb4b97e7
Mehdi Boustani <sckapa.bst@gmail.com>
2025-02-10 22:14:39 +0100

    fix: database infinite loading

--- formation/lib/main.dart
+++ formation/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:formation/data/note_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'pages/note_list_page.dart';
 
 void main() {
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+
   runApp(ChangeNotifierProvider(
     create: (context) => NoteDatabaseProvider(),
     child: const MyApp(),
